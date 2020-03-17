9d3f9e66a916268e29c0bba95e581188f4db6d95
rasmus <danzel89@gmail.com>
2020-03-17 20:50:48 +0200

    ex00 session working

--- ex00/index.php
+++ ex00/index.php
@@ -1,25 +1,19 @@
 <?php
-if ($_GET['login'])
-{
-	$login = $_GET['login'];
-	echo "login $login\n";
-}
-if ($_GET['passwd'])
-{
-	$pw = $_GET['passwd'];
-	echo "password $pw\n";
-}
+
+session_start();
+
 if ($_GET['submit'] && $_GET['submit'] === 'OK')
 {
-	$submit = $_GET['submit'];
-	echo "submit $submit\n";
+	$_SESSION['login'] = $_GET['login'];
+	$_SESSION['pw'] = $_GET['passwd'];
 }
+
 ?>
 <html><body>
 <form method="get">
-Username: <input type="text" name="login" value="<?= $login?>" />
+Username: <input type="text" name="login" value="<?= $_SESSION['login']?>" />
 <br />
-Password: <input type="text" name="password" value="<?= $pw?>" />
-<input type="submit" name="submit" value="<?= $submit?>" />
+Password: <input type="text" name="passwd" value="<?= $_SESSION["pw"]?>" />
+<input type="submit" name="submit" value="OK" />
 </form>
 </body></html>
