1992920d35aaa67ca7f29763081edf54df257f7e
rasmus <danzel89@gmail.com>
2020-03-18 23:42:06 +0200

    added OK messages and trimmed unnecessary functions

--- ex01/create.php
+++ ex01/create.php
@@ -17,29 +17,25 @@ else
 
 	if (file_exists($path) === FALSE)
 		mkdir($path);
-
-	$user[login] = $_POST['login'];
-	$user[passwd] = hash('sha256', 'suolaa' . $_POST['passwd']);
-
 	if (file_exists($file) === TRUE)
 	{
 		$data = file_get_contents($file);
 		$arr = unserialize($data);
 	}
-	if (empty($arr))
-		$arr = array($user);
-	else
+	$last = -1;
+	foreach($arr as $key => $value)
 	{
-		foreach($arr as $key => $value)
-		{
-			if ($value[login] === $user[login])
-				return_error();
-		}
-		array_push($arr, $user);
+		if ($value[login] === $_POST['login'])
+			return_error();
+		$last++;
 	}
+	$arr[$last + 1]['login'] = $_POST['login'];
+	$arr[$last + 1]['passwd'] = hash('sha256', 'suolaa' . $_POST['passwd']);
+	
 
 	$data = serialize($arr);
 	file_put_contents($file, $data);
+	echo "OK\n";
 }
 
 ?>
--- ex02/modif.php
+++ ex02/modif.php
@@ -13,38 +13,28 @@ if ($_POST['submit'] !== 'OK' || !$_POST['login'] || !$_POST['oldpw']|| !$_POST[
 else
 {
 	$file = '../private/passwd';
-
-	if (file_exists($file) === FALSE)
-		return_error();
-
 	$user = $_POST['login'];
 
-	if (file_exists($file) === TRUE)
+	$data = file_get_contents($file);
+	$arr = unserialize($data);
+
+	foreach($arr as $key => $value)
 	{
-		$data = file_get_contents($file);
-		$arr = unserialize($data);
-	}
-	if (empty($arr))
-		return_error();
-	else
-	{
-		foreach($arr as $key => $value)
+		if ($value[login] === $user)
 		{
-			if ($value[login] === $user)
+			if ($value[passwd] === hash('sha256', 'suolaa' . $_POST['oldpw']))
 			{
-				if ($value[passwd] === hash('sha256', 'suolaa' . $_POST['oldpw']))
-				{
-					$arr[$key][passwd] = hash('sha256', 'suolaa' . $_POST['newpw']);
-					$data = serialize($arr);
-					file_put_contents($file, $data);
-					exit();
-				}
-				else
-					return_error();
+				$arr[$key][passwd] = hash('sha256', 'suolaa' . $_POST['newpw']);
+				$data = serialize($arr);
+				file_put_contents($file, $data);
+				echo "OK\n";
+				exit();
 			}
+			else
+				return_error();
 		}
-		return_error();
 	}
+	return_error();
 }
 
 ?>
