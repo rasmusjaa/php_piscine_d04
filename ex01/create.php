8048d45da53502bf07a92ab1786819bec1316e05
rasmusjaa <rasmus@lense.fi>
2020-04-18 21:05:18 +0300

    removed empty line

--- ex01/create.php
+++ ex01/create.php
@@ -31,7 +31,6 @@ else
 	}
 	$arr[$last + 1]['login'] = $_POST['login'];
 	$arr[$last + 1]['passwd'] = hash('sha256', 'suolaa' . $_POST['passwd']);
-	
 
 	$data = serialize($arr);
 	file_put_contents($file, $data);
